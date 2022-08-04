641fbb6bb9e7370b8513e15d6ba42ee9f5d62ace
kdongk <ldh5081@naver.com>
2022-08-04 19:47:18 +0900

    로그인 patch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,9 +11,12 @@ function login() {
         id : id.value,
         psword : psword.value,
     };
-    console.log(req);
-    console.log(JSON.stringify((req)));
-    // fetch('/login', {
-    //     body: JSON.stringify(req)
-    // });
+
+    fetch('/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 }
